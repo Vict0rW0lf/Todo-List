c4346fed687c76d72bd51289092d54bcc1bf5ac7
diowolf <diorgenes_tex@live.com>
2019-07-24 22:09:21 -0300

    Updates list and fixes bug

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -11,15 +11,13 @@ document.addEventListener('DOMContentLoaded', function(){
 		myNodelist[i].appendChild(span);
 	}
 	
-// Click on a close button to hide the current list item
-	var close = document.getElementsByClassName("close");
-	var i;
-	for (i = 0; i < close.length; i++) {
-		close[i].onclick = function() {
-			var div = this.parentElement;
-			div.style.display = "none";
-		}
-	}
+// Click on a close button to remove the current list item
+	
+	$("#myUL").on("click", ".close", e => {
+		var li = $(e.target).parent();
+		deleteItem(li.attr("id"));
+		li.remove();
+	});
 	
 // Add a "checked" symbol when clicking on a list item
 	var list = document.querySelector('ul');
@@ -29,32 +27,60 @@ document.addEventListener('DOMContentLoaded', function(){
 		}
 	}, false);
 	
-	
+}, false);
+
 // Create a new list item when clicking on the "Add" button
-	function newElement() {
-		var li = document.createElement("li");
-		var inputValue = document.getElementById("myInput").value;
-		var t = document.createTextNode(inputValue);
-		li.appendChild(t);
-		if (inputValue === '') {
-			alert("You must write something!");
-		} else {
-			document.getElementById("myUL").appendChild(li);
-		}
-		document.getElementById("myInput").value = "";
-		
-		var span = document.createElement("SPAN");
-		var txt = document.createTextNode("\u00D7");
-		span.className = "close";
-		span.appendChild(txt);
-		li.appendChild(span);
-		
-		for (i = 0; i < close.length; i++) {
-			close[i].onclick = function() {
-				var div = this.parentElement;
-				div.style.display = "none";
+function newElement() {
+	var li = document.createElement("li");
+	var inputValue = document.getElementById("myInput").value;
+	var t = document.createTextNode(inputValue);
+	li.appendChild(t);
+	if (inputValue === '') {
+		alert("You must write something!");
+	} else {
+		$.when(saveItem(inputValue)).done(function(result) {
+
+			if (result) {
+				
+				document.getElementById("myUL").prepend(li);
+				var span = document.createElement("SPAN");
+				var txt = document.createTextNode("\u00D7");
+				span.className = "close";
+				span.appendChild(txt);
+				li.appendChild(span);
+				li.setAttribute("id", result.id);
+				
+				for (i = 0; i < close.length; i++) {
+					close[i].onclick = function() {
+						var div = this.parentElement;
+						div.style.display = "none";
+					}
+				}
 			}
-		}
+			
+		});
+		
 	}
 	
-}, false);
+	document.getElementById("myInput").value = "";
+}
+
+function saveItem(itemDescription) {
+	const json = `{"description": "${itemDescription}", "isDone": false}`;
+	
+	return $.ajax({
+        url: "item",
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        data: json
+    });
+}
+
+function deleteItem(id) {
+	$.ajax({
+        url: `item/${id}`,
+        type: "DELETE",
+        success: function(d) {
+        }
+    });
+}
