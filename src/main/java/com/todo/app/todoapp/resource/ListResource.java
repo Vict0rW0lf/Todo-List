3a803dc5e89ff958ec597b362b28dae130d831cb
diowolf <diorgenes_tex@live.com>
2019-07-24 22:10:23 -0300

    Updates resource

--- src/main/java/com/todo/app/todoapp/resource/ListResource.java
+++ src/main/java/com/todo/app/todoapp/resource/ListResource.java
@@ -1,13 +1,22 @@
 package com.todo.app.todoapp.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.todo.app.todoapp.service.ItemService;
 
 @Controller
 public class ListResource {
 
+	@Autowired
+	private ItemService itemService;
+	
 	@GetMapping("/")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("itens", itemService.getAllItems());
+		
 		return "home";
 	}
 	
