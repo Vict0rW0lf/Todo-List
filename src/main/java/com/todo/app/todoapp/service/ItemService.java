e240218a340c4f1f6b4b3d343cbd4226e1f4d4dc
diowolf <diorgenes_tex@live.com>
2019-07-24 22:11:04 -0300

    Updates item service

--- src/main/java/com/todo/app/todoapp/service/ItemService.java
+++ src/main/java/com/todo/app/todoapp/service/ItemService.java
@@ -24,5 +24,24 @@ public class ItemService {
 		return itens;
 	}
 	
+	public Item saveItem(Item item) {
+		return itemRepo.save(item);
+	}
+	
+	public Item replaceItem(Item item) {
+		return itemRepo.save(item);
+	}
+	
+	public Item updateItem(Item item) {
+		return itemRepo.save(item);
+	}
+	
+	public void deleteItemById(long id) {
+		itemRepo.deleteById(id);
+	}
+
+	public void deleteAll() {
+		itemRepo.deleteAll();
+	}
 	
 }
